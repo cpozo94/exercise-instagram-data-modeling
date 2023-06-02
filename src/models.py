f6ed50d44c8e825346f57fa4c27895d578436680
Carlos Pozo Guijarro <cpg.948@gmail.com>
2023-06-02 17:47:22 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,39 +8,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250))
-    firstname = Column(String(250))
-    lastname = Column(String(250))
-    email = Column(Integer)
-
-class Media(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(String(250))
-    url = Column(String(250))
-    post_id = Column(Integer)
-    
+    id = Column(Integer,  primary_key=True, nullable=False)
+    username = Column(String(100), nullable=False)
+    firstname = Column(String(100), nullable=False) 
+    lastname = Column(String(100), nullable=False)
+    email = Column(String(100), primary_key=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    followers = relationship('Follower')
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    id = Column (Integer,  primary_key=True, nullable=False)
+    post_date = Column (Date, nullable=False)
+    image = Column (String(250), nullable=False)
+    description = Column(Text())
+    user_id= Column(String, ForeignKey('user.id'))
+    user=relationship(User)
 
 class Comment(Base):
     __tablename__ = 'comment'
+    id = Column (Integer,  primary_key=True, nullable=False)
+    comment_text = Column (Text(), nullable=False)
+    comment_date = Column (Date, nullable=False)
+    post_id = Column(String, ForeignKey('post.id'))
+    follower_id = Column(String, ForeignKey('follower.id'))
+    post= relationship('Post')
+    follower= relationship('Follower')
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer)
-    post_id = Integer
-    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
 
     
 
